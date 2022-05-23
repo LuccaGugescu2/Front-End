1b652cc72f92140ea83b50d36231db83fbf09473
lucca gugescu <luccagugescu@gmail.com>
2022-05-23 21:16:32 +0200

    add navigation setup and relative paths

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,33 @@
 // @generated: @expo/next-adapter@2.1.52
 // Learn more: https://docs.expo.dev/guides/using-nextjs/
 
-module.exports = { presets: ['@expo/next-adapter/babel'] };
+module.exports = {
+    presets: ['@expo/next-adapter/babel'], plugins: ['react-native-reanimated/plugin',
+        [
+            "module-resolver",
+            {
+                root: ["./"],
+                extensions: [
+                    '.ios.ts',
+                    '.android.ts',
+                    '.ts',
+                    '.ios.tsx',
+                    '.android.tsx',
+                    '.tsx',
+                    '.jsx',
+                    '.js',
+                    '.json',
+                    ".web.js"
+                ],
+                alias: {
+                    "@components": ["./components"],
+                    "@hooks": ["./hooks"],
+                    "@utils": ["./utils"],
+                    "@public": ["./public"],
+                    "@interfaces": ["./interfaces"],
+                    "@navigation": ["./interfaces"]
+                },
+            },
+        ],
+    ],
+};
